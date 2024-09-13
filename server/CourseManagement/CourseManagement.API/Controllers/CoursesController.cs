b6b7d0dc34a8a7703a2ee25936cbbcd288a68f83
Bruno Giovagnoli <brunogiovagnoli@gmail.com>
2024-09-13 04:34:30 -0300

    creating the CoursesController with a single endpoint for fetching the courses, with an optional parameter for Description

---
+++ server/CourseManagement/CourseManagement.API/Controllers/CoursesController.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using CourseManagement.Application.Features.Courses.Dtos;
+using CourseManagement.Application.Features.Courses.Queries.GetCourses;
+
+namespace CourseManagement.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoursesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CoursesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        /// <summary>
+        /// Retrieves a list of courses. Optionally filters by description.
+        /// </summary>
+        /// <param name="description">Optional description to filter courses by.</param>
+        /// <returns>A list of courses.</returns>
+        /// <response code="200">Returns the list of courses</response>
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<CourseDto>), 200)]
+        public async Task<IActionResult> GetCourses([FromQuery] string? description = null)
+        {
+            var query = new GetCoursesQuery { Description = description };
+            var courses = await _mediator.Send(query);
+            return Ok(courses);
+        }
+    }
+}
--- server/CourseManagement/CourseManagement.API/Program.cs
+++ server/CourseManagement/CourseManagement.API/Program.cs
@@ -1,6 +1,7 @@
 using CourseManagement.Application.Extensions;
 using CourseManagement.Infrastructure.Data;
 using CourseManagement.Infrastructure.Extensions;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,7 +14,12 @@ builder.Services.AddInfrastructure(builder.Configuration.GetConnectionString("De
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
