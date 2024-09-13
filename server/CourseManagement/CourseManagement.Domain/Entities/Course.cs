eb4e1277182ad7e64e2eec56d881464ead14975a
Bruno Giovagnoli <brunogiovagnoli@gmail.com>
2024-09-13 02:23:56 -0300

    creating a db context with the Course entity that includes basic constraints for its columns

---
+++ server/CourseManagement/CourseManagement.Domain/Entities/Course.cs
@@ -0,0 +1,9 @@
+﻿namespace CourseManagement.Domain.Entities;
+
+public class Course
+{
+    public int Id { get; set; }
+    public required string Subject { get; set; }
+    public required string CourseNumber { get; set; }
+    public required string Description { get; set; }
+}
---
+++ server/CourseManagement/CourseManagement.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CourseManagement.Domain.Entities;
+
+namespace CourseManagement.Infrastructure.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Course> Courses { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Course>(entity =>
+        {
+            entity.HasIndex(e => new { e.Subject, e.CourseNumber }).IsUnique();
+
+            entity.Property(e => e.CourseNumber)
+                    .IsRequired()
+                    .HasMaxLength(3)
+                    .IsFixedLength();
+
+            entity.Property(e => e.Subject)
+                    .IsRequired();
+
+            entity.Property(e => e.Description)
+                   .IsRequired();
+        });
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
