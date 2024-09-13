39dbc1ce31603c10ffe6d109934bde0323b99b93
Bruno Giovagnoli <brunogiovagnoli@gmail.com>
2024-09-13 04:05:32 -0300

    creating first migration to update the schema of the local database instance

---
+++ server/CourseManagement/CourseManagement.Infrastructure/Migrations/20240913070149_initialCreate.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace CourseManagement.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Courses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Subject = table.Column<string>(type: "text", nullable: false),
+                    CourseNumber = table.Column<string>(type: "character(3)", fixedLength: true, maxLength: 3, nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Courses", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_Subject_CourseNumber",
+                table: "Courses",
+                columns: new[] { "Subject", "CourseNumber" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Courses");
+        }
+    }
+}
