7ff3363fdee695fdae5fcdd95fb54b23bac71ffc
Bruno Giovagnoli <brunogiovagnoli@gmail.com>
2024-09-13 03:00:49 -0300

    adding ValidationBehavior to handle validation errors, registering DI for MediatR and FluentValidation packages for application layer

--- server/CourseManagement/CourseManagement.API/Program.cs
+++ server/CourseManagement/CourseManagement.API/Program.cs
@@ -1,3 +1,4 @@
+using CourseManagement.Application.Extensions;
 using CourseManagement.Infrastructure.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration.GetConnectionString("DefaultConnection"));
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ server/CourseManagement/CourseManagement.Application/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using FluentValidation;
+using CourseManagement.Application.Behaviors;
+
+namespace CourseManagement.Application.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+        return services;
+    }
+}
