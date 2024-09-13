1f41eda92f1fe7ca8a12c3ca21949f353877a428
Bruno Giovagnoli <brunogiovagnoli@gmail.com>
2024-09-13 11:19:00 -0300

    instaling Polly for retrying db connection (to make it work using Docker)

--- server/CourseManagement/CourseManagement.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ server/CourseManagement/CourseManagement.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.DependencyInjection;
 using CourseManagement.Application.Interfaces;
 using CourseManagement.Infrastructure.Data;
 using CourseManagement.Infrastructure.Repositories;
+using Microsoft.Extensions.Options;
 
 namespace CourseManagement.Infrastructure.Extensions;
 
@@ -11,7 +12,16 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, string connectionString)
     {
         services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseNpgsql(connectionString));
+        {
+            options.UseNpgsql(connectionString, npgsqlOptions =>
+            {
+                npgsqlOptions.EnableRetryOnFailure(
+                    maxRetryCount: 5,
+                    maxRetryDelay: TimeSpan.FromSeconds(10),
+                    errorCodesToAdd: null
+                );
+            });
+        });
 
         services.AddScoped<ICourseRepository, CourseRepository>();
 
