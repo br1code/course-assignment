a336e455e5019543824b2afcc667347d96c2cb5e
Bruno Giovagnoli <brunogiovagnoli@gmail.com>
2024-09-13 04:23:41 -0300

    initializing the database at startup time and seeding it with some courses

--- server/CourseManagement/CourseManagement.API/Program.cs
+++ server/CourseManagement/CourseManagement.API/Program.cs
@@ -1,4 +1,5 @@
 using CourseManagement.Application.Extensions;
+using CourseManagement.Infrastructure.Data;
 using CourseManagement.Infrastructure.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,6 +16,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+await DatabaseInitializer.SeedAsync(app.Services);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
---
+++ server/CourseManagement/CourseManagement.Infrastructure/Data/DatabaseInitializer.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using CourseManagement.Domain.Entities;
+
+namespace CourseManagement.Infrastructure.Data;
+
+public static class DatabaseInitializer
+{
+    public static async Task SeedAsync(IServiceProvider services)
+    {
+        using var scope = services.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        try
+        {
+            Console.WriteLine("Initializing database.");
+
+            await context.Database.MigrateAsync();
+            await SeedCourses(context);
+
+            Console.WriteLine("Database initialized successfully.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred while seeding the database: {ex.Message}");
+            throw;
+        }
+    }
+
+    private static async Task SeedCourses(ApplicationDbContext context)
+    {
+        if (!context.Courses.Any())
+        {
+            Console.WriteLine("Seeding the database with courses ...");
+
+            context.Courses.AddRange(
+                new Course { Subject = "BIO", CourseNumber = "101", Description = "Introduction to Biology" },
+                new Course { Subject = "MAT", CourseNumber = "045", Description = "Business Statistics" }
+            );
+
+            await context.SaveChangesAsync();
+        }
+    }
+}
